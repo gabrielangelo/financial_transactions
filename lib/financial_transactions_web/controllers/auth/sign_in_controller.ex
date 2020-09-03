bec6f56f52b3be21823df1c61e8a5190025bc25d
gabriel <gabri005@gmail.com>
2020-09-02 23:40:11 -0300

    UPDATE: added auth controller

---
+++ lib/financial_transactions_web/controllers/auth/sign_in_controller.ex
@@ -0,0 +1,19 @@
+defmodule FinancialTransactionsWeb.SignInController do
+  use FinancialTransactionsWeb, :controller
+  alias FinancialTransactionsWeb.Guardian
+
+  def create(conn, %{"email" => email, "password" => password}) do
+    case  FinancialTransactions.Tasks.SignIn.run(email, password) do
+      {:ok, user} ->
+        {:ok, token, _} = Guardian.encode_and_sign(user)
+        render(conn, "login.json", %{token: token})
+
+      {:error, _} ->
+        conn
+        |> put_status(401)
+        |> json(%{status: "unauthenticated"})
+
+      end
+  end
+
+end
