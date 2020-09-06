340c8f749a50e0c43a07044d1cd856951777fda5
gabriel <gabri005@gmail.com>
2020-09-06 01:13:33 -0300

    UPDATE: removed alter accounts migrations and edited migration that sets date default date to transaction

--- priv/repo/migrations/20200826173931_alter_accounts.exs
+++
@@ -1,9 +0,0 @@
-defmodule FinancialTransactions.Repo.Migrations.AlterAccounts do
-  use Ecto.Migration
-
-  def change do
-    alter table(:accounts) do
-      add :current_balance, :decimal, default: 0.0
-    end
-  end
-end
