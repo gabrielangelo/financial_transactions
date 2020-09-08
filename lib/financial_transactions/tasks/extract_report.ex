c0761eea5c4de7dcc54dec4d1369f7d54603723c
gabriel <gabri005@gmail.com>
2020-09-08 19:42:17 -0300

    UPDATE: added report date everlaps validation

--- lib/financial_transactions/tasks/extract_report.ex
+++ lib/financial_transactions/tasks/extract_report.ex
@@ -1,19 +1,55 @@
 defmodule FinancialTransactions.Tasks.ExtractReport do
   import Ecto.Query, only: [from: 2]
+  import Ecto.Changeset, only: [add_error: 3]
+
   alias FinancialTransactions.Accounts.Transaction
   alias FinancialTransactions.Repo
 
-  def run(account_id, %Date{} = start_date_range, %Date{} = end_date_range) do
-    process(account_id, start_date_range, end_date_range)
+  defp check_range_dates_formats(%Date{} = start_date_range, %Date{} = end_date_range) do
+    {start_date_range, end_date_range}
   end
 
-  def run(account_id, start_date_range, end_date_range)
-  when is_binary(start_date_range) and is_binary(end_date_range) do
-    {{:ok, start_date_range}, {:ok,end_date_range}} = {Date.from_iso8601(start_date_range), Date.from_iso8601(end_date_range)}
-    process(account_id, start_date_range, end_date_range)
+  defp check_range_dates_formats(start_date_range, end_date_range)
+    when is_binary(start_date_range) and is_binary(end_date_range) do
+      {
+        {:ok, start_date_range},
+        {:ok,end_date_range}
+      } = {Date.from_iso8601(start_date_range), Date.from_iso8601(end_date_range)}
+
+      {start_date_range, end_date_range}
   end
 
-  defp process(account_id, start_date_range, end_date_range) do
+  defp check_range_dates_order(dates) do
+    {start_date_range, end_date_range} = dates
+    case Date.compare(start_date_range, end_date_range) do
+      :gt -> {
+        :error,
+        add_error(
+          %Ecto.Changeset{},
+          :start_date_range,
+          "start_date_range cannot be greater then end_date_range"
+        )
+      }
+      _ -> {:ok, start_date_range, end_date_range}
+    end
+  end
+
+  defp validate_dates(start_date_range, end_date_range) do
+    check_range_dates_formats(start_date_range, end_date_range)
+    |> check_range_dates_order
+  end
+
+  def run(account_id, start_date_range, end_date_range) do
+    case validate_dates(start_date_range, end_date_range) do
+      {:ok, start_date, end_date} ->
+        process_extrat_report(account_id, start_date, end_date)
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  defp process_extrat_report(account_id, start_date_range, end_date_range) do
     query_out = from(
      t in Transaction,
       where: (
@@ -23,6 +59,7 @@ defmodule FinancialTransactions.Tasks.ExtractReport do
          and t.from_account_id == ^account_id
       ),
       group_by: t.emit_at,
+      order_by: t.emit_at,
       select: %{emit_at: t.emit_at, total: sum(t.value)}
     )
     query_in = from(
@@ -34,6 +71,7 @@ defmodule FinancialTransactions.Tasks.ExtractReport do
         and t.to_account_id == ^account_id
       ),
       group_by: t.emit_at,
+      order_by: t.emit_at,
       select: %{emit_at: t.emit_at, total: sum(t.value)}
     )
 
