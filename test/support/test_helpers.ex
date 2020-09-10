a17c3ea4d0a0410f918713becee0cb57fa376be1
gabriel <gabri005@gmail.com>
2020-09-10 00:01:23 -0300

    UPDATE: added build_authenticate_conns

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -1,7 +1,12 @@
 defmodule FinancialTransactions.TestHelpers do
   alias FinancialTransactions.Companies.Company
   alias FinancialTransactions.Users.User
+  alias FinancialTransactions.Accounts.Account
+
   alias FinancialTransactions.Repo
+
+  import Plug.Conn
+  alias FinancialTransactionsWeb.Guardian
 
   def build_attrs(:user_with_account_initial_value) do
     %{
@@ -15,6 +20,15 @@ defmodule FinancialTransactions.TestHelpers do
           current_balance: 1000.0,
         },
       ],
+    }
+  end
+
+  def build_attrs(:user_without_accounts) do
+    %{
+      email: "non_email@gmail.com",
+      first_name: "Simon",
+      last_name: "B.A Phanton",
+      password: "123456Gg",
     }
   end
 
@@ -50,6 +64,16 @@ defmodule FinancialTransactions.TestHelpers do
     }
   end
 
+  def build_attrs(:staff_user) do
+    %{
+      first_name: "Jack",
+      last_name: "London",
+      password: "123456Gg",
+      is_staff: true,
+      email: "staff@gmail.com"
+    }
+  end
+
   def company_fixture() do
     {:ok, company} =
     %Company{}
@@ -59,8 +83,8 @@ defmodule FinancialTransactions.TestHelpers do
     company |> Repo.preload(:accounts)
   end
 
-  def user_with_account_fixture(attrs \\ build_attrs(:user_with_account_initial_value)) do
-    company_fixture()
+  @spec user_fixture(:invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}) :: any
+  def user_fixture(attrs \\ %{}) do
     {:ok, user} =
     %User{}
     |> User.changeset(attrs)
@@ -68,4 +92,31 @@ defmodule FinancialTransactions.TestHelpers do
     user
   end
 
+  def account_fixture(attrs \\ %{}) do
+    {:ok, account} =
+    %Account{}
+    |> Account.changeset(attrs)
+    |> Repo.insert
+    account
+  end
+
+  def authenticate(conn, user) do
+    {:ok, token, _} = Guardian.encode_and_sign(user)
+    put_req_header(conn, "authorization", "Bearer " <> token)
+  end
+
+  def build_authenticate_conns(conn) do
+    staff_user = user_fixture(build_attrs(:staff_user))
+    staff_conn = authenticate(conn, staff_user)
+
+    non_staff_user =  user_fixture(build_attrs(:user_without_accounts))
+    non_staff_conn = authenticate(conn, non_staff_user)
+
+    {:ok,
+      %{staff_conn: put_req_header(staff_conn, "accept", "application/json"),
+      non_staff_conn: put_req_header(non_staff_conn, "accept", "application/json"),
+      staff_user: staff_user,
+      non_staff_user: non_staff_user}
+    }
+  end
 end
