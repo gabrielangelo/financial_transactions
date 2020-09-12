8150fe57a0383ded20010a0bb24c3c724e359324
gabriel <gabri005@gmail.com>
2020-09-12 17:10:16 -0300

    UPDATE: added default values

--- lib/financial_transactions/repo.ex
+++ lib/financial_transactions/repo.ex
@@ -5,10 +5,10 @@ defmodule FinancialTransactions.Repo do
 
     def init(_, config) do
       config = config
-        |> Keyword.put(:username, System.get_env("DB_USER"))
-        |> Keyword.put(:password, System.get_env("DB_PASSWORD"))
-        |> Keyword.put(:database, System.get_env("DATABASE_DB"))
-        |> Keyword.put(:hostname, System.get_env("DB_HOST"))
+        |> Keyword.put(:username, System.get_env("DB_USER") || "postgres")
+        |> Keyword.put(:password, System.get_env("DB_PASSWORD") || "postgres")
+        |> Keyword.put(:database, System.get_env("DATABASE_DB") || "financial_transactions_dev")
+        |> Keyword.put(:hostname, System.get_env("DB_HOST") || "localhost")
         |> Keyword.put(:port, System.get_env("DB_PORT") || 5432)
       {:ok, config}
     end
